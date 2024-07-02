71f24891f818368a65d63ec65687ada6a3c222e5
gabrielmarafon <gabrielmarafon5@gmail.com>
2024-07-02 22:22:54 +0000

    Última atualização

--- src/BellmanFord/App.java
+++ src/BellmanFord/App.java
@@ -7,33 +7,29 @@ import java.util.List;
 public class App {
     public static void main(String[] args) {
         List<Vertex> vertexList = new ArrayList<>();
-        Vertex va = new Vertex("A");
-        Vertex vb = new Vertex("B");
-        Vertex vc = new Vertex("C");
-        Vertex vd = new Vertex("D");
-        Vertex ve = new Vertex("E");
+        Vertex vs = new Vertex("S");
+        Vertex vr = new Vertex("R");
+        Vertex vt = new Vertex("T");
+        Vertex vu = new Vertex("U");
 
-        vertexList.add(va);
-        vertexList.add(vb);
-        vertexList.add(vc);
-        vertexList.add(vd);
-        vertexList.add(ve);
+        vertexList.add(vs);
+        vertexList.add(vr);
+        vertexList.add(vt);
+        vertexList.add(vu);
 
         List<Edge> edgeList = new ArrayList<>();
-        edgeList.add(new Edge(-1, va, vb));
-        edgeList.add(new Edge(4, va, vc));
-        edgeList.add(new Edge(3, vb, vc));
-        edgeList.add(new Edge(2, vb, vd));
-        edgeList.add(new Edge(2, vb, ve));
-        edgeList.add(new Edge(1, vd, vb));
-        edgeList.add(new Edge(5, vd, vc));
-        edgeList.add(new Edge(-3, ve, vd));
+        edgeList.add(new Edge(3, vs, vt));
+        edgeList.add(new Edge(1, vs, vu));
+        edgeList.add(new Edge(3, vr, vs));
+        edgeList.add(new Edge(5, vr, vu));
+        edgeList.add(new Edge(7, vr, vt));
+        edgeList.add(new Edge(1, vu, vt));
 
         BellmanFord bellmanFord = new BellmanFord(vertexList, edgeList);
-        bellmanFord.bellmanFord(va);
+        bellmanFord.bellmanFord(vs);
 
         for (Vertex vertex : vertexList) {
-            System.out.println("Shortest path from A to " + vertex.getName() + ": " + bellmanFord.getShortestPathTo(vertex));
+            System.out.println("Shortest path from S to " + vertex.getName() + ": " + bellmanFord.getShortestPathTo(vertex));
         }
     }
 }
